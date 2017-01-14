b46234d7482848277b7f82669dce2c4cf5db16d2
David Sheldrick <david@futurice.com>
2017-01-14 20:43:51 +0100

    up to talking about derived state managment

--- presentation/index.js
+++ presentation/index.js
@@ -7,6 +7,7 @@ import {
   BlockQuote,
   Cite,
   CodePane,
+  Code,
   Deck,
   Fill,
   Heading,
@@ -19,7 +20,8 @@ import {
   Quote,
   Slide,
   Spectacle,
-  Text
+  Text,
+  S
 } from "spectacle";
 
 // Import image preloader util
@@ -48,10 +50,11 @@ const images = {
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081",
+  primary: "#ec607f",
   pink: "#ec607f",
   yellow: "#f6d743",
-  green: "#48f3a5"
+  green: "#48f3a5",
+  grey: "#999"
 }, {
   primary: "Sharp Sans No1",
   secondary: { name: "Droid Serif", googleFont: true, styles: [ "400", "700i" ] }
@@ -69,111 +72,224 @@ export default class Presentation extends React.Component {
             <Heading size={1} fit textColor="pink" caps>
               Secret sauce
             </Heading>
-            <Link href="https://twitter.com/djsheldrick">
-              <Text display="inline" textColor="white">
+            <Text display="inline" textColor="white">
+              <Link href="https://twitter.com/djsheldrick" display="inline" textColor="white">
                 <Image display="inline" src={images.twitter} width={100} margin="0 0 -35px" />
                 @djsheldrick
+              </Link>
+              <Link href="https://github.com/ds300" display="inline" textColor="white">
                 <Image display="inline" src={images.github} width={120} margin="0 0 -17px" padding="0 24px" />
                 @ds300
-              </Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
+              </Link>
+            </Text>
           </Slide>
           <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
+            <Heading size={2} caps fit textColor="yellow">
+              premise
             </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
+            <Heading caps textColor="white">
+              React is clever and good
             </Heading>
           </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
+          <Slide transition={["slide"]} bgColor="black" notes="want to talk about it with technical specificity">
+            <Heading size={2} caps fit textColor="yellow">
+              Why?
+            </Heading>
+            <Heading caps textColor="white">
+              What's the big idea?
+            </Heading>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps fit textColor="yellow">
+              SO what?
+            </Heading>
+            <Heading caps textColor="white">
+              Can we leverage the same insight elsewhere?
+            </Heading>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="yellow">
+            <Heading size={1} caps fit textColor="pink">
+              Simple State Management
+            </Heading>
+            <Text textColor="black" padding="40px 0" bold>
+              enables
+            </Text>
+            <Heading size={1} caps fit textColor="pink">
+              Simple Effects Management
+            </Heading>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="yellow">
+            <Text>
+              <strong>What the heck is state</strong> and how do we manage it?
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="yellow">
+            <Heading size={1} caps textColor="pink">
+              State is
+            </Heading>
+            <Text padding="25px 0">
+              <strong>Information about the world</strong> [ data ]
+            </Text>
+            <Text padding="25px 0">
+              <strong>Inert, malleable, continuous</strong>
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={6} caps textColor="white" fontSize="80px">
+              type 1
+            </Heading>
+            <Heading size={3} caps textColor="green" fontSize="80px">
+              Constant State
+            </Heading>
+            <Text textColor="white">
+              [ &nbsp;couldn't be more boring&nbsp; ]
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={6} caps textColor="white" fontSize="80px">
+              type 2
+            </Heading>
+            <Heading size={3} caps textColor="green" fontSize="80px">
+              Stack state
+            </Heading>
+            <List textColor="white">
+              <ListItem>Function arguments</ListItem>
+              <ListItem>Loop variables</ListItem>
+              <ListItem>Intermediate results</ListItem>
+              <ListItem>...</ListItem>
+            </List>
+            <Text textColor="white" padding="20px 0">
+              [ &nbsp;easy to manage&nbsp; ]
+            </Text>
+            <Text textColor="white">
+              [ &nbsp;use tastefully restrictive tools&nbsp; ]
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="yellow">
+            <Text>
+              Some apps are basically functions and get by with
+              <strong> only constant and stack state.</strong>
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="yellow">
+            <Text>
+              Most apps <strong>sit around waiting for external events </strong>
+              to trigger internal behaviour.
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={3} caps textColor="pink" fontSize="80px">
+              EVENTS
+            </Heading>
+            <Text textColor="white">
+              [ &nbsp;things that happened&nbsp; ]
+            </Text>
+            <List textColor="white">
+              <ListItem>Data about single points in time</ListItem>
+              <ListItem>Invasive forces</ListItem>
+              <ListItem>Used to trigger <strong>Effects</strong></ListItem>
             </List>
           </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={3} caps textColor="pink" fontSize="80px">
+              EFFECTS
             </Heading>
-            <Interactive/>
+            <Text textColor="white" padding="0px 0px 30px">
+              [ &nbsp;things that happen<s style={{opacity: 0.2}}>ed</s>&nbsp; ]
+            </Text>
+            <Text textColor="white" textAlign="left">
+              Either:
+            </Text>
+            <List textColor="white">
+              <ListItem>Persistent changes to internal state</ListItem>
+              <ListItem>Behaviour in other systems</ListItem>
+            </List>
           </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
+          <Slide transition={["slide"]} bgColor="yellow">
+            <Markdown>
+              What to call this **persistent, changing internal state**?
+            </Markdown>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={6} caps textColor="white" fontSize="80px">
+              type 3
             </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+            <Heading size={3} caps textColor="green" fontSize="80px">
+              Heap state
+            </Heading>
+            <List textColor="white">
+              <ListItem>Kept in memory <em>on the heap</em> </ListItem>
+              <ListItem>Changed in response to events</ListItem>
+              <ListItem>Changes persist across call stacks</ListItem>
+            </List>
+            <Text textColor="white" padding="20px 0">
+              [ &nbsp;still quite easy to manage&nbsp; ]
+            </Text>
+            <Text textColor="white">
+              [ &nbsp;FP, Event Sourcing, STM, Actors, CSP...&nbsp; ]
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={6} textColor="white" fontSize="80px">
+              TYPE 3a
+            </Heading>
+            <Heading size={3} caps textColor="green" fontSize="80px">
+              Atomic state
+            </Heading>
+            <Text textColor="white">
+              [ &nbsp;normalized app data&nbsp; ]
+            </Text>
+            <List textColor="white">
+              <ListItem>Ignoring perf, the minimum information your app needs to function correctly.</ListItem>
+              <ListItem>Must not be computable by looking at other bits of state.</ListItem>
+            </List>
+            <Text textColor="white" padding="20px 0">
+              [ &nbsp;e.g. count, todos,
+            </Text>
+            <Text textColor="white">
+              mouseX, db cache, ...&nbsp; ]
+            </Text>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={6} textColor="white" fontSize="80px">
+              TYPE 4
+            </Heading>
+            <Heading size={3} caps textColor="green" fontSize="80px">
+              Derived state
+            </Heading>
+            <Text textColor="white">
+              [ &nbsp;<code><span style={{color: theme.screen.colors.pink}}>derive</span><span style={{color: theme.screen.colors.grey}}>(</span><span style={{color: theme.screen.colors.yellow}}>atomicState</span><span style={{color: theme.screen.colors.grey}}>)</span></code>&nbsp; ]
+            </Text>
+            <Text textColor="white" textAlign="left">
+              e.g.
+            </Text>
+            <List textColor="white">
+              <ListItem>Whether or not an input form is valid</ListItem>
+              <ListItem>The number of idle users in an IRC channel</ListItem>
+              <ListItem>The pixel width of a <code>div</code> whose width is specified in percent</ListItem>
+            </List>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="pink">
+            <Heading size={4} textColor="black" fontSize="80px">
+              <span style={{color: "white"}}>We mostly suck</span> at managing derived state
+            </Heading>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="black">
+            <Heading size={6} textColor="white" fontSize="80px">
+              Approach 1
+            </Heading>
+            <Heading size={3} caps textColor="pink" fontSize="80px">
+              Not managing it
+            </Heading>
+            <Text textColor="white">
+              [ &nbsp; re-computing before each use &nbsp; ]
+            </Text>
+            <List textColor="white">
+              <ListItem>Means all derived state is also stack state</ListItem>
+              <ListItem>Lots of extra CPU/GC usage</ListItem>
+              <ListItem>Difficult to stay DRY</ListItem>
+              <ListItem>Poor separation of concerns</ListItem>
+            </List>
           </Slide>
         </Deck>
       </Spectacle>
